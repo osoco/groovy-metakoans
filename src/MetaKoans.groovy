35ba51d3bc02ceed49180aa9de852d7e74d8b13b
Marcin Gryszko <marcin@gryszko.net>
2011-08-05 22:17:02 +0200

    Next actions modified

--- src/MetaKoans.groovy
+++ src/MetaKoans.groovy
@@ -7,6 +7,7 @@ import org.junit.runners.Suite.SuiteClasses
 class MetaKoans {
     // TODO Koans - 12.3 Dynamically Accessing Objects
     // TODO Koans - 14., complete chapter
+    // TODO Transform koans into cloze tests (substitute /*koanify*//**/ markers with __
 
     // TODO how HandleMetaClass and DelegatingMetaClass work? Why are they used instead of ExpandoMetaClass?
     // TODO groovy.lang.MetaClassRegistry? How it works? What is it for?
